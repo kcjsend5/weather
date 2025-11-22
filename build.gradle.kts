5fa52c75aa2ecda3b638e69e958275e7bca66128
kcjsend5 <kcjsend5@naver.com>
2025-11-22 14:29:44 +0900

    테스트 4

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,7 +43,6 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.kafka:spring-kafka-test")
     testImplementation("org.springframework.security:spring-security-test")
-    testRuntimeOnly("com.mysql:mysql-connector-j")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
