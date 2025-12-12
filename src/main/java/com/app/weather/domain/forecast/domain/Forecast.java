1c87217a8d95433124c15e762e3dcde8db534358
kcjsend5 <kcjsend5@naver.com>
2025-12-12 12:23:08 +0900

    오류

--- src/main/java/com/app/weather/domain/forecast/domain/Forecast.java
+++ src/main/java/com/app/weather/domain/forecast/domain/Forecast.java
@@ -25,7 +25,7 @@ public class Forecast extends BaseEntity {
 
     private int fcstTime;
 
-    @OneToMany(mappedBy = "weather",cascade = CascadeType.ALL,orphanRemoval = true)
+    @OneToMany(mappedBy = "forecast",cascade = CascadeType.ALL,orphanRemoval = true)
     @Builder.Default
     private List<Measurement> measurements = new ArrayList<>();
 
